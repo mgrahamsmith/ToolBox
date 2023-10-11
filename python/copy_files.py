41050e550bf427db93bddc30fb301e3df200e96a
Graham Smith <mgrahamsmith@protonmail.com>
2023-10-11 00:36:21 -0500

    Reformatting comments.

--- python/copy_files.py
+++ python/copy_files.py
@@ -1,7 +1,15 @@
+################################################################################
+##
+## Copy files recursively from one directory to another.  If the directory 
+## specified to be copied to does not exist, it will be created.  All failed
+## copy operations will be reported in a text file containing the list of files 
+## that failed to be copied.
+##
+################################################################################
+
 import argparse
 
-# Template argparse code from https://docs.python.org/3/library/argparse.html
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser(description='Copy files from one directory to another.')
 parser.add_argument('integers', metavar='N', type=int, nargs='+',
                     help='an integer for the accumulator')
 parser.add_argument('--sum', dest='accumulate', action='store_const',
