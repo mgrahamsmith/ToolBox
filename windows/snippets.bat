7eb386d1ad324019ad97e869f6bf32ac89ff2ab6
Graham Smith <mgs3@email.arizona.edu>
2023-10-24 16:38:02 -0500

    Update snippets.bat

--- windows/snippets.bat
+++ windows/snippets.bat
@@ -11,3 +11,12 @@ if exist folder_name (
 ) else (
   echo NO
 )
+
+:: Check whether the current environment is under WSL
+::
+:: Source: https://superuser.com/questions/1749781/how-can-i-check-if-the-environment-is-wsl-from-a-shell-script
+
+if [ -f "/etc/wsl.conf" ]; then
+   . "$HOME/.bash.d/wsl"
+fi
+
