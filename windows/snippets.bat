bbaff3e893cbf8d3b6ddc2b5ef18f6c9ae1e754a
Graham Smith <mgs3@email.arizona.edu>
2023-10-24 16:42:15 -0500

    Update snippets.bat

--- windows/snippets.bat
+++ windows/snippets.bat
@@ -5,6 +5,7 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Check if file / folder exists:
+
 @echo off 
 if exist folder_name (
   echo YES
@@ -14,7 +15,11 @@ if exist folder_name (
 
 :: Alternate way to check if a file exists.
 ::
-:: Example: Check whether the current environment is under WSL
+:: Example: Check whether the current environment is under Windows Subsystem for
+:: Linux, since WSLInterop should only exist when running WSL.
+:: 
+:: Tested with WSL2, running Ubuntu-18.04
+::
 :: Source: https://superuser.com/questions/1749781/how-can-i-check-if-the-environment-is-wsl-from-a-shell-script
 
 if [ -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]; then
